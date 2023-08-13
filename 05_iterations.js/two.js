920bb4233da945510d822f7bcab867482cc910ea
atulsikarwargit <140576092+atulsikarwargit@users.noreply.github.com>
2023-08-13 04:05:07 +0000

    do while loop

---
+++ 05_iterations.js/two.js
@@ -0,0 +1,34 @@
+// let i = 0;
+// while (i <= 10) {
+//     console.log(`value of i ${i}`);
+//     i = i+2;
+// }
+let myArray = ["flash", 'stop', 'start', 'green']
+let arr = 0;
+while (arr <myArray.length) {
+   // console.log(`value is ${myArray[arr]}`);
+    arr = arr + 1;
+//    console.log(myArray.length); 
+}
+
+
+let  number = 21
+do {
+    console.log(`student is ${number}`);
+    // number =  number + 1; or
+    number ++;
+} while (number < 20);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
